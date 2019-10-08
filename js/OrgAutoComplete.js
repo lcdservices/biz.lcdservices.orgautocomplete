d1fd7da9befe395d950d2083f13b7afb62e8b6c9
David Hayes <david@blackbrick.software>
2019-10-08 18:51:40 -0500

    fix tabbing

--- js/OrgAutoComplete.js
+++ js/OrgAutoComplete.js
@@ -2,39 +2,39 @@ CRM.$(function($) {
   $('#oac-wrapper').insertAfter('#current_employer');
 
   $("#org_switcher").click(function(){
-  	var data = $(this).text();
-		$(this).text(data === 'Select Organization' ? 'Add New Organization' : 'Select Organization');
+    var data = $(this).text();
+    $(this).text(data === 'Select Organization' ? 'Add New Organization' : 'Select Organization');
 
-		if (data === "Add New Organization"){
+    if (data === "Add New Organization"){
       $('#current_employer').val('').show();
       $('div#oac-org-select').val('').hide();
 
       //$('#current_employer').val("");
-			//$('#s2id_current_employer').hide();
-			//$('.select2-choice').addClass('select2-default');
-			//$('.select2-chosen').text('- Select Organization -');
-			//$('label[for="employer_id"]').hide();
-			//$('#organization_name').show();
-			//$('label[for="organization_name"]').show();
-		}
+      //$('#s2id_current_employer').hide();
+      //$('.select2-choice').addClass('select2-default');
+      //$('.select2-chosen').text('- Select Organization -');
+      //$('label[for="employer_id"]').hide();
+      //$('#organization_name').show();
+      //$('label[for="organization_name"]').show();
+    }
 
-		if (data === "Select Organization"){
+    if (data === "Select Organization"){
       $('#current_employer').val('').hide();
       $('div#oac-org-select').val('').show();
 
-			//$("#organization_name").val('').hide();
-			//$('label[for="organization_name"]').hide();
-			//$('#s2id_current_employer').show();
-			//$('label[for="employer_id"]').show();
-		}
-	});
+      //$("#organization_name").val('').hide();
+      //$('label[for="organization_name"]').hide();
+      //$('#s2id_current_employer').show();
+      //$('label[for="employer_id"]').show();
+    }
+  });
 
   //default display select field/hide text field
   $('#current_employer').val('').hide();
   $('div#oac-org-select').val('').show();
 
   //$('#organization_name').width(220).hide();
-	//$('label[for="organization_name"]').hide();
-	//$('#s2id_current_employer').show();
-	//$('label[for="employer_id"]').show();
+  //$('label[for="organization_name"]').hide();
+  //$('#s2id_current_employer').show();
+  //$('label[for="employer_id"]').show();
 });
